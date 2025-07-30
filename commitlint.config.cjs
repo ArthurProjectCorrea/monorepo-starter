aef86ad8d819494db98f13156e156456dd1abd27
arthurdepaulacorrea@hotmail.com <arthurdepaulacorrea@hotmail.com>
2025-07-30 11:59:35 -0400

    chore(monorepo): update commitlint config for scope enforcement and commit rules

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,24 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'api',
+        'web',
+        'docs',
+        'ui',
+        'eslint-config',
+        'tailwind-config',
+        'typescript-config',
+        '.changeset',
+        '.github',
+        '.husky',
+        '.vscode',
+        'scripts',
+        'monorepo'
+      ]
+    ]
+  }
+};
